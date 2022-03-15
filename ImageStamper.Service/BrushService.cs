23575e1c6aec8673d6ffb29a0a7e88697d6da3cd
Ben Osborne <osborne.ben@gmail.com>
2022-03-15 08:43:35 -0500

    created brush service

---
+++ ImageStamper.Service/BrushService.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ImageStamper.Service
+{
+    public class BrushService
+    {
+        public SolidBrush GetSolidBrush(string colorName) => 
+            new(GetBrushColor(colorName));
+
+        protected Color GetBrushColor(string colorName)
+        {
+            var brushColor = Color.FromName(colorName);
+            if(brushColor.IsKnownColor) return brushColor;
+
+            return colorName.Contains("#")
+                ? ColorTranslator.FromHtml(colorName)
+                : ColorTranslator.FromHtml($"#{colorName}");
+        }
+    }
+}
