597179669e278bfa087654fc9458199d7e7259e9
Ben Osborne <osborne.ben@gmail.com>
2022-03-15 11:15:59 -0500

    Created ImageConverter and StampSizeService

---
+++ ImageStamper.Service/ImageConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace ImageStamper.Service
+{
+    public class ImageConverter
+    {
+        public Graphics BitmapToGraphics(Bitmap bitmapIn)
+        {
+            var imgGrfx = Graphics.FromImage((Bitmap)bitmapIn.Clone());
+            imgGrfx.CompositingQuality = CompositingQuality.HighQuality;
+            imgGrfx.SmoothingMode = SmoothingMode.HighQuality;
+            imgGrfx.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            return imgGrfx;
+        }
+    }
+}
---
+++ ImageStamper.Service/StampSizeService.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace ImageStamper.Service
+{
+    public class StampSizeService
+    {
+        public SizeF GetStampSize(
+            Func<Graphics> getGraphicsDelegate,
+            string text,
+            Font font,
+            double targetWidth
+            )
+        {
+            int fontSize = 1;
+            Graphics imgGrphx = getGraphicsDelegate.Invoke();
+            var size = imgGrphx.MeasureString(text, font);
+
+            // keep increasing font until the stamp satisfies target size
+            while (size.Width < targetWidth)
+            {
+                using var newfont = new Font(font.FontFamily, fontSize, FontStyle.Bold);
+                size = imgGrphx.MeasureString(text, font);
+            }
+
+            return size;
+        }
+    }
+}
