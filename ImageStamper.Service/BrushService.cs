12c022d9a0091b4fcc5685782740967243a5fdb7
Ben Osborne <osborne.ben@gmail.com>
2022-03-15 08:52:14 -0500

    Refactored brushservice

--- ImageStamper.Service/BrushService.cs
+++ ImageStamper.Service/BrushService.cs
@@ -7,14 +7,18 @@ namespace ImageStamper.Service
         public SolidBrush GetSolidBrush(string colorName) => 
             new(GetBrushColor(colorName));
 
-        protected Color GetBrushColor(string colorName)
-        {
-            var brushColor = Color.FromName(colorName);
-            if(brushColor.IsKnownColor) return brushColor;
+        protected Color GetBrushColor(string colorName) =>
+            TryGetKnownColor(colorName) ?? GetColorFromHtml(colorName);
 
-            return colorName.Contains("#")
+        protected Color? TryGetKnownColor(string colorName)
+        {
+            var color = Color.FromName(colorName);
+            return color.IsKnownColor ? color : null;
+        }
+
+        protected Color GetColorFromHtml(string colorName) =>
+            colorName.Contains("#")
                 ? ColorTranslator.FromHtml(colorName)
                 : ColorTranslator.FromHtml($"#{colorName}");
-        }
     }
 }
