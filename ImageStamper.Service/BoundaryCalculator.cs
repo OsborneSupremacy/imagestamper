6eafe039a41e4756ef194ce30d09d3ce5be69d51
Ben Osborne <osborne.ben@gmail.com>
2022-03-15 16:54:22 -0500

    created BoundaryCalculator

---
+++ ImageStamper.Service/BoundaryCalculator.cs
@@ -0,0 +1,24 @@
+﻿using ImageStamper.Objects;
+
+namespace ImageStamper.Service
+{
+    public class BoundaryCalculator
+    {
+        public Boundaries Calculate(
+            float imageWidth,
+            float imageHeight,
+            float stampWidth,
+            float stampHeight,
+            float padPixels
+            ) => 
+            new()
+            {
+                Left = padPixels,
+                XCenter = (imageWidth / 2) - (stampWidth / 2),
+                Right = imageWidth - (stampWidth + padPixels),
+                Top = padPixels,
+                YCenter = (imageHeight / 2) - (stampHeight / 2),
+                Bottom = imageHeight - (stampHeight + padPixels)
+            };
+    }
+}
